f5f2afb0c4d33d1d168c7c080c71911a861969c7
Maycon <maycondouglasniculau@gmail.com>
2026-01-30 10:20:54 -0300

    Refatora a função MinorQuest para melhorar a legibilidade e organiza o código da função SortingbySelection

--- MinorQuestSortingbyselection/MinorQuestSortingbyselection.py
+++ MinorQuestSortingbyselection/MinorQuestSortingbyselection.py
@@ -2,8 +2,9 @@
 # Page 54 from the book "Grokking Algorithms" by Aditya Bhargava
 # Pagina 54 do livro "Entendendo Algoritmos" por Aditya Bhargava
 
-
-def MinorQuest(arr):  # Function to find the index of the smallest element in the array # Funcao para encontrar o indice do menor elemento no array
+# Function to find the index of the smallest element in the array 
+# Funcao para encontrar o indice do menor elemento no array
+def MinorQuest(arr): 
 
     smaller = arr[0]   # Assume the fist element is the smallest # Assume que o primeiro elemento e o menor
     smaller_index = 0  # Initialize the index of the smallest element # Inicializa o indice do menor elemento
@@ -21,13 +22,16 @@ def MinorQuest(arr):  # Function to find the index of the smallest element in th
 
     return smaller_index     # Return the index of the smallest element # Retorna o indice do menor elemento
 
-
+# Function to sort the array using selection sort algorithm
+# Funcao para ordenar o array usando o algoritmo de ordenacao por selecao
 def SortingbySelection(arr):
-    newArr = []
-    for i in range(len(arr)):
-        smaller = MinorQuest(arr)
-        newArr.append(arr.pop(smaller))
-    return newArr
+
+    newArr = []  # Initialize a new array to hold the sorted elements # Inicializa um novo array para guardaa os elementos ordenados 
+
+    for i in range(len(arr)):            # Loop through the original array # Percorre o array original 
+        smaller = MinorQuest(arr)        # Find the index of the smallest element # Encontra o indice do menor elemento
+        newArr.append(arr.pop(smaller))  # Append the smallest element to the new array and remove it from the original array
+    return newArr                        # Return the sorted array # Retorna o array ordenado
 
 print(SortingbySelection([5, 3, 6, 2, 10]))  
 
